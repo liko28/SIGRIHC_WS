6da61e4277419be1636b352c9cba88c3830fe609
Ramiro Alvarez <liko28s@gmail.com>
2016-11-08 14:05:50 -0500

    - Se añade clave primaria para buscar facilmente por el indice

--- models/UserModel.php
+++ models/UserModel.php
@@ -7,5 +7,7 @@ class UsersModel extends BaseModel {
     public function __construct(Connection $connection) {
         parent::__construct($connection);
         $this->setTableName("USERS");
+        $this->setPrimaryKey("ID");
     }
+
 }
