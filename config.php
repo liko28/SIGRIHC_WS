798d84950c6f466fb8d7f8d805a98d0aaaaefdd0
Ramiro Alvarez <liko28s@gmail.com>
2016-11-08 10:34:12 -0500

    - Config, donde se mantendran las globales y constantes de configuracion

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramiro
+ * Date: 8/11/16
+ * Time: 10:26 AM
+ */
+const CONNECTION_CREDENTIALS = array('BD' => 'SALUD', 'HOST' => '192.168.1.247', 'PORT' => 50000, 'USERNAME' => 'db2inst1', 'PASSWORD' => 'db2inst1');
