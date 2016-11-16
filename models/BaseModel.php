23706b4cff587bf6d7e3db195596f677006883e6
Ramiro Alvarez <liko28s@gmail.com>
2016-11-16 12:11:01 -0500

    - Refactor de Base Model para ajustarse a los cambios que realicé en la conexion y el abandono de adodb-php (Por lento)

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -2,29 +2,43 @@
 
 namespace Models;
 
-class BaseModel {
+use Helpers\CustomArray;
+use Helpers\Row;
+
+class BaseModel{
+    /** @var null|string  */
     protected $tableName;
+    /** @var null|string  */
     protected $schema;
+    /** @var mixed|null  */
     protected $primaryKey;
+    /** @var Connection  */
     protected $connection;
+    /** @var  string */
     protected $query;
+    /** @var CustomArray  */
     protected $result;
+    /** @var  CustomArray */
+    protected $columns;
 
     /**
+     * BaseModel constructor.
      * @param Connection $connection
-     * @param string $tableName
-     * @param string $schema
-     * @param mixed $primaryKey
+     * @param null|string $tableName
+     * @param null|string $schema
+     * @param null|array $primaryKey
      */
-    function __construct(Connection $connection, $schema = null, $tableName = null, $primaryKey = null) {
-        $this->setTableName($tableName);
-        $this->setSchema($schema ? $schema : $connection->getDefaultSchema());
-        $this->setConnection($connection);
-        $this->setPrimaryKey($primaryKey);
+    function __construct(Connection $connection, $tableName = null, $schema = null, $primaryKey = null) {
+        $this->tableName = $tableName;
+        $this->schema = $schema ? $schema : $connection->getDefaultSchema();
+        $this->connection = $connection;
+        $this->primaryKey = $primaryKey;
+        $this->result = new CustomArray();
+        $this->columns = new CustomArray();
     }
 
     /**
-     * @return string
+     * @return null|string
      */
     public function getTableName()
     {
@@ -32,7 +46,7 @@ class BaseModel {
     }
 
     /**
-     * @param string $tableName
+     * @param null|string $tableName
      */
     public function setTableName($tableName)
     {
@@ -40,7 +54,7 @@ class BaseModel {
     }
 
     /**
-     * @return string
+     * @return null|string
      */
     public function getSchema()
     {
@@ -48,11 +62,27 @@ class BaseModel {
     }
 
     /**
-     * @param string $schema
+     * @param null|string $schema
      */
     public function setSchema($schema)
     {
         $this->schema = $schema;
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getPrimaryKey()
+    {
+        return $this->primaryKey;
+    }
+
+    /**
+     * @param mixed|null $primaryKey
+     */
+    public function setPrimaryKey($primaryKey)
+    {
+        $this->primaryKey = $primaryKey;
     }
 
     /**
@@ -87,9 +117,8 @@ class BaseModel {
         $this->query = $query;
     }
 
-
     /**
-     * @return mixed
+     * @return CustomArray
      */
     public function getResult()
     {
@@ -97,34 +126,98 @@ class BaseModel {
     }
 
     /**
-     * @param mixed $result
+     * @param CustomArray $result
      */
-    public function setResult($result)
+    public function setResult(CustomArray $result)
     {
         $this->result = $result;
     }
 
     /**
-     * @return mixed
-     */
-    public function getPrimaryKey()
+     * @return array
+     * @throws \ErrorException
+     * */
+    public function getColumns(...$columns)
     {
-        return $this->primaryKey;
+        foreach ($columns as $column) {
+            $filteredColumns = new CustomArray();
+            if(array_key_exists($column,$this->getColumns())) {
+                $filteredColumns[$column] = $column;
+            } else {
+                throw new \ErrorException("El Campo $column NO EXISTE EN LA TABLA $this->schema.$this->tableName");
+            }
+        }
+        if(!$columns) {
+            return $this->columns;
+        }
+        return $filteredColumns;
     }
 
     /**
-     * @param mixed $primaryKey
+     * @param CustomArray $columns
      */
-    public function setPrimaryKey($primaryKey)
+    public function setColumns(CustomArray $columns)
     {
-        $this->primaryKey = $primaryKey;
+        $this->columns = $columns;
     }
 
-    public function query($sql) {
-        return $this->getConnection()->getConnectionResource()->query($sql);
+    /** @param string */
+    public function addColumns(...$columns) {
+        foreach ($columns as $column) {
+            $this->columns[$column] = $column;
+        }
     }
 
-    public function getRow($sql) {
-        return \JsonSerializable::
+    public function query($SQLsentence,...$arguments) {
+        if($this->connection->getConnectionResource()) {
+            $preparedStmt = db2_prepare($this->connection->getConnectionResource(),$SQLsentence);
+            foreach ($arguments as $index => $argument) {
+                $parameters[] = $argument;
+            }
+            if($preparedStmt) {
+                if($this->execute($preparedStmt,$parameters)) {
+                    $operation = substr($SQLsentence,0,strpos($SQLsentence," "));
+                    switch ($operation) {
+                        case "INSERT":
+                            //Caso si Insert
+                            return db2_last_insert_id($preparedStmt);
+                            break;
+                        case "SELECT":
+                            //Caso si Select
+                            if(LABELS) {
+                                while ($result = db2_fetch_assoc($preparedStmt)) {
+                                    $this->result->append(new Row($result));
+                                }
+                            } else {
+                                while ($result = db2_fetch_array($preparedStmt)) {
+                                    $this->result->append($result);
+                                }
+                            }
+                            return $this->result;
+                            break;
+                        case "UPDATE":
+                            //Caso si Update
+                            break;
+                        default:
+                            return array("hola");
+                        break;
+                    }
+                }else {
+                    throw new \ErrorException("ERROR DE EJECUCION ".db2_stmt_error().":".db2_stmt_errormsg());
+                }
+            } else {
+                throw new \ErrorException("ERROR DE PREPARACION ".db2_stmt_error().":".db2_stmt_errormsg());
+            }
+        } else {
+            throw new \ErrorException("ERROR DE CONEXION ".db2_conn_error().": ".db2_conn_errormsg());
+        }
+    }
+
+    public function execute($preparedStmt,$parameters = null){
+        if(is_null($parameters)) {
+            return db2_execute($preparedStmt);
+        } else {
+            return db2_execute($preparedStmt,$parameters);
+        }
     }
 }
