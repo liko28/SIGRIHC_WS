8fb9f039ad1015d86e55b1a0d55e0fc7ddd3e8fc
Ramiro Alvarez <liko28s@gmail.com>
2016-11-16 12:12:24 -0500

    - Refactorizacion de los metodos de UserModel para ajustarse a BaseModel

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -8,13 +8,14 @@ class UsersModel extends BaseModel {
         parent::__construct($connection);
         $this->setTableName("USERS");
         $this->setPrimaryKey("ID");
+        $this->addColumns("ID", "NOMBRE", "PASSWORD", "TIPO_USUARIO", "ACTIVO", "EMAIL", "DPTO", "PAIS", "CIUDAD", "MOVIL", "TELEFONO", "DIRECCION", "DOC_IDENT", "NOMBRES", "APELLIDOS", "CARGO", "TIPO_DOC", "INFORMA_A", "FECHA_CREA", "USER_CREA", "IP_CREA", "FECHA_MODI", "USER_MODI", "IP_MODI");
     }
 
     public function getByUserName($userName) {
-        return $this->getConnection()->getConnectionResource()->getRow("SELECT * FROM {$this->getSchema()}.{$this->getTableName()} WHERE NOMBRE = ?",$userName);
+        return $this->query("SELECT {$this->getColumns()->commaSep()} FROM {$this->getSchema()}.{$this->getTableName()} WHERE NOMBRE = '?'",$userName);
     }
 
     public function getPassword($userName) {
-        return $this->getConnection()->getConnectionResource()->getRow("SELECT PASSWORD FROM {$this->getSchema()}.{$this->getTableName()} WHERE NOMBRE = ?", $userName);
+        return $this->query("SELECT {$this->getColumns('PASSWORD')->commaSep()} FROM {$this->getSchema()}.{$this->getTableName()} WHERE NOMBRE = ?",$userName);
     }
 }
