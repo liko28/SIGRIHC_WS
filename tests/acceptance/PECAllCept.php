18f542b28959c1857a532ff85f5c2bbe1e145a63
Ramiro Alvarez <liko28s@gmail.com>
2016-11-25 15:27:17 -0500

    - Tests Para PEC (Corregidos)

--- tests/acceptance/PECAllCept.php
+++ tests/acceptance/PECAllCept.php
@@ -3,27 +3,27 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('Verificar que Datos de PEC - All - Funciona');
 $I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
 //Grupos Objetivo
-$I->sendGET('/PEC/GruposObjetivo/tipo/get/all');
+$I->sendGET('/PEC/GruposObjetivo/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_OBJETIVO');
 //Guias
-$I->sendGET('/PEC/Guias/lista/get/all');
+$I->sendGET('/PEC/Guias/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_GUIA');
 //Procesos
-$I->sendGET('/PEC/Procesos/lista/get/all');
+$I->sendGET('/PEC/Procesos/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_PROCESO');
 //Programacion
-$I->sendGET('/PEC/Programacion/lista/get/all');
+$I->sendGET('/PEC/Programacion/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID');
 //Temas
-$I->sendGET('/PEC/Temas/lista/get/all');
+$I->sendGET('/PEC/Temas/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_TEMA');
--- tests/acceptance/PECUpdatesCept.php
+++ tests/acceptance/PECUpdatesCept.php
@@ -3,12 +3,12 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('Verificar que Datos de PEC - Updates - Funciona');
 $I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
 //Guias
-$I->sendGET('/PEC/Guias/lista/get/updates/1478001600');
+$I->sendGET('/PEC/Guias/get/updates/1478001600');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_GUIA');
 //Temas
-$I->sendGET('/PEC/Temas/lista/get/updates/1478001600');
+$I->sendGET('/PEC/Temas/get/updates/1478001600');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
 $I->seeResponseJsonMatchesJsonPath('$.[*].ID_TEMA');
