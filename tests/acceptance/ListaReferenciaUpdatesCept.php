17ef8287e79fcbee49a3d0a0ed0f03433c85bdb4
Ramiro Alvarez <liko28s@gmail.com>
2016-11-25 14:38:02 -0500

    - Tests para Lista de Referencia

---
+++ tests/acceptance/ListaReferenciaUpdatesCept.php
@@ -0,0 +1,8 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Verificar que Lista de Referencia Funciona');
+$I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
+$I->sendGET('/ListaReferencia/get/updates/1478001600');
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$.[*].ID_LISTA');
