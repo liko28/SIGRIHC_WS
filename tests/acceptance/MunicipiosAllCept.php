1ccc8caab8fe6b76da179ac2910031851ea06dd0
Ramiro Alvarez <liko28s@gmail.com>
2016-11-25 14:54:31 -0500

    - Tests Para Municipios

---
+++ tests/acceptance/MunicipiosAllCept.php
@@ -0,0 +1,8 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Verificar que Lista de Municipios - All - Funciona');
+$I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
+$I->sendGET('/Municipios/get/all');
+$I->seeResponseCodeIs(200);
+$I->seeResponseIsJson();
+$I->seeResponseJsonMatchesJsonPath('$.[*].ID_CIUDAD');
