9ddf73ba2f872f24a71b7c10ae64c54272fb4109
Ramiro Alvarez <liko28s@gmail.com>
2016-11-25 14:54:16 -0500

    - Así se ve mejor :)

--- tests/acceptance/ListaReferenciaAllCept.php
+++ tests/acceptance/ListaReferenciaAllCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Verificar que Lista de Referencia Funciona');
+$I->wantTo('Verificar que Lista de Referencia - All - Funciona');
 $I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
 $I->sendGET('/ListaReferencia/get/all');
 $I->seeResponseCodeIs(200);
--- tests/acceptance/ListaReferenciaUpdatesCept.php
+++ tests/acceptance/ListaReferenciaUpdatesCept.php
@@ -1,6 +1,6 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Verificar que Lista de Referencia Funciona');
+$I->wantTo('Verificar que Lista de Referencia - Updates - Funciona');
 $I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
 $I->sendGET('/ListaReferencia/get/updates/1478001600');
 $I->seeResponseCodeIs(200);
