e2aaee76970272abf54e143dfb8c77ab51bd6f39
Ramiro Alvarez <liko28s@gmail.com>
2016-11-28 09:45:10 -0500

    - Test para Laboratorios - Pequeña Correccion para medicamentos

--- tests/acceptance/LaboratoriosAllCept.php
+++ tests/acceptance/LaboratoriosAllCept.php
@@ -1,8 +1,8 @@
 <?php
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Verificar que Lista de Medicamentos - All - Funciona');
+$I->wantTo('Verificar que Lista de Laboratorios - All - Funciona');
 $I->amHttpAuthenticated("yenny.navarro","0e9c305be2086dddde743735105aceb5");
-$I->sendGET('/Medicamentos/get/all');
+$I->sendGET('/Laboratorios/get/all');
 $I->seeResponseCodeIs(200);
 $I->seeResponseIsJson();
-$I->seeResponseJsonMatchesJsonPath('$.[*].ID_MEDICAMENTO');
+$I->seeResponseJsonMatchesJsonPath('$.[*].ID_LABORATORIO');
