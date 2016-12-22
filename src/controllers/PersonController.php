ef2b16c506e93973acc4066bcd4c7735c82550a1
Ramiro Alvarez <liko28s@gmail.com>
2016-12-22 12:27:01 -0500

    - Me di cuenta que esto estaba mal nombrado

--- src/controllers/PersonController.php
+++ src/controllers/PersonController.php
@@ -3,11 +3,11 @@
 namespace SIGRI_HC\Controllers;
 
 use SIGRI_HC\Models\Connection;
-use SIGRI_HC\Models\PersonaModel;
+use SIGRI_HC\Models\PersonModel;
 
 class PersonController extends BaseController {
     /** @param Connection $connection */
     public function __construct(Connection $connection) {
-        parent::__construct(new PersonaModel($connection));
+        parent::__construct(new PersonModel($connection));
     }
 }
