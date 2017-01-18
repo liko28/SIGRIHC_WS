ad09cda4b852d834fea21cd57af2871a5b1ef4b3
Ramiro Alvarez <liko28s@gmail.com>
2017-01-18 07:31:40 -0500

    - HcMedica se divide en bloques que agrupan los registros multiples.

--- src/controllers/HcMedicaController.php
+++ src/controllers/HcMedicaController.php
@@ -23,38 +23,100 @@ class HcMedicaController extends BaseController {
         $result = array();
 
         foreach ($stories as $person => $answers) {
-            foreach ($answers as $answer) {
+            /** PROCESAMIENTO DEL BLOQUE RESPUESTAS */
+            foreach ($answers['RESPUESTAS'] as $answer) {
                 $entity = $questions->getQuestionEntity($answer[0]);
                 if(!isset($entities[$entity]) && ($entity !== "SF_PERSONAS" || $entity !== "SF_NPERSONAS")) {
                     $entities[$entity] = new Row();
                 }
-                $entities[$entity]->addField([$questions->getQuestionField($answer[0]) => $answer[1]]);
+                $tipo = $questions->getQuestionType($answer[0]);
+                $respuesta = null;
+                switch ($tipo) {
+                    case "BIGINT":
+                    case "SMALLINT":
+                    case "INTEGER":
+                        $respuesta = intval($answer[1]);
+                        break;
+                    case "DECIMAL":
+                        $respuesta = floatval($answer[1]);
+                        break;
+                    case "VARCHAR":
+                    case "DATE":
+                    case "TIMESTAMP":
+                    case "TIME":
+                        $respuesta = $answer[1];
+                        break;
+                }
+                $entities[$entity]->addField([$questions->getQuestionField($answer[0]) => $respuesta]);
             }
 
-            //TODO ESTE ES UN ERROR EN LA TABLA PREGUNTAS
+            /** PROCESAMIENTO DEL BLOQUE ACOMPAÑANTES */
+            foreach ($answers['ACOMPAÑANTES'] as $companion => $answers) {
+                foreach ($answers as $answer) {
+                    $entity = $questions->getQuestionEntity($answer[0]);
+                    if(!isset($entities[$entity]->$companion) && ($entity !== "SF_PERSONAS" || $entity !== "SF_NPERSONAS")) {
+                        $entities[$entity]->$companion = new Row();
+                    }
+                    $tipo = $questions->getQuestionType($answer[0]);
+                    $respuesta = null;
+                    switch ($tipo) {
+                        case "BIGINT":
+                        case "SMALLINT":
+                        case "INTEGER":
+                            $respuesta = intval($answer[1]);
+                            break;
+                        case "DECIMAL":
+                            $respuesta = floatval($answer[1]);
+                            break;
+                        case "VARCHAR":
+                        case "DATE":
+                        case "TIMESTAMP":
+                        case "TIME":
+                            $respuesta = $answer[1];
+                            break;
+                    }
+                    $entities[$entity]->$companion->addField([$questions->getQuestionField($answer[0]) => $respuesta]);
+                }
+            }
+
+
+            /** INSERCION DE HC_MEDICA (PARENT) */
+            //TODO ESTE ES UN ERROR EN LA TABLA PREGUNTAS, DEBERIA TENER TODOS LOS CAMPOS
             $entities['HC_MEDICA']->addField(["ID_USUARIO"=>$person,"TIPOHC"=>"ME","ESTADO" => "A","PROGRAMACION" => 1, "RIESGOCV" => 1]);
             try{
                 $hcId = $this->model->insert($entities['HC_MEDICA']);
             } catch (\Exception $e) {
-                $hcId['ERROR'] = $e->getMessage();
-                $result[] = $hcId;
+                //TODO Log error
+                $result[] = ['ERROR' => $e->getMessage()];
+                db2_rollback($this->model->getConnection()->getConnectionResource());
                 continue;
-            } finally {
-                if(!$e) {
-                    db2_commit($this->model->getConnection()->getConnectionResource());
-                } else {
-                    db2_rollback($this->model->getConnection()->getConnectionResource());
-                }
             }
+            /** ELIMINACION DE LOS DATOS DE HC_MEDICA PARA EVITAR UNA NUEVA INSERCION */
             unset($entities['HC_MEDICA']);
 
+            /** INSERCION DEL RESTO DE REGISTROS */
             foreach ($entities as $entity => $row) {
                 switch ($entity){
+                    case "HC_RESPONSABLE":
+                        foreach ($row as $companion) {
+                            //TODO Inject Fecmodi, IpModi, Feccrea, Ipcrea, Usercrea, Usermodi
+                            //TODO Inject UserId
+                            $baseModel->setTableName($entity);
+                            $companion->addField(["ID_HC" => $hcId]);
+                            try {
+                                $baseModel->insert($companion);
+                            } catch (\Exception $e) {
+                                //TODO Log error
+                                $result[] = ['ERROR' => $e->getMessage()];
+                                db2_rollback($this->model->getConnection()->getConnectionResource());
+                                continue 3;
+                            }
+                        }
+                        break;
                     case "HC_ANTFALLECIDO":
                     case "HC_ANTFAMILIAR":
                     case "HC_ANTFAMILIARTC":
                     case "HC_ANTGINECO":
-                    case "HC_ANTHABITOS":
                     case "HC_ANTMEDICAMENTOS":
                     case "HC_ANTPERSONAL":
                     case "HC_ANTPERSONAL1":
@@ -69,13 +131,13 @@ class HcMedicaController extends BaseController {
                     case "HC_EXAMENLAB":
                     case "HC_EXAMENLABO":
                     case "HC_GESCITAS":
+                    case "HC_HABITOS":
                     case "HC_INTERCONSULTA":
                     case "HC_MEDICA":
                     case "HC_PARACLINICOS":
                     case "HC_PECTEMAS":
                     case "HC_PLANTERAPEUTICO":
                     case "HC_REGISTROEXA":
-                    case "HC_RESPONSABLE":
                     case "HC_REVISION":
                     case "HC_TESTBAR":
                     case "HC_TESTBAR":
@@ -85,20 +147,17 @@ class HcMedicaController extends BaseController {
                         $row->addField(["ID_HC" => $hcId]);
                         try {
                             $baseModel->insert($row);
-                            Logger::log(200,$baseModel->getQuery());
                         } catch (\Exception $e) {
-                            $hcId['ERROR'] = $e->getMessage();
-                        } finally {
-                            if(!$e) {
-                                db2_commit($this->model->getConnection()->getConnectionResource());
-                            } else {
-                                db2_rollback($this->model->getConnection()->getConnectionResource());
-                            }
+                            //TODO Log error
+                            $result[] = ["ERROR" => $e->getMessage()];
+                            db2_rollback($this->model->getConnection()->getConnectionResource());
+                            continue 3;
                         }
                         break;
                 }
             }
             $result[] = $hcId;
+            db2_commit($this->model->getConnection()->getConnectionResource());
         }
         return $result;
     }
