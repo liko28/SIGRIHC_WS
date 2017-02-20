de94ee8c5d683236be061b4a7ff1b8b2533c7994
Ramiro Alvarez <liko28s@gmail.com>
2017-02-20 15:13:40 -0500

    - Documentacion Actualizada

--- doc/api_project.js
+++ doc/api_project.js
@@ -56,7 +56,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-02-20T15:48:18.274Z",
+    "time": "2017-02-20T19:51:08.941Z",
     "url": "http://apidocjs.com",
     "version": "0.16.1"
   }
