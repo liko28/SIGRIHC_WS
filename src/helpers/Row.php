6b1326fbe499a4ce1ff124014471752c917bcd03
Ramiro Alvarez <liko28s@gmail.com>
2017-04-07 10:07:55 -0500

    - Correccion de Caracteres

--- src/helpers/Row.php
+++ src/helpers/Row.php
@@ -9,13 +9,13 @@ class Row {
      */
     public function __construct(array $fields = null) {
         foreach ($fields as $field => $value) {
-            $this->$field = utf8_decode($value);
+            $this->$field = utf8_encode($value);
         }
     }
 
     public function addField(array $field){
         foreach ($field as $_field => $value) {
-                $this->$_field = utf8_decode($value);
+                $this->$_field = utf8_encode($value);
         }
     }
 }
