17fe13df066c4bb3e819c7618c392da018ce36ad
Ramiro Alvarez <liko28s@gmail.com>
2017-05-09 15:55:44 -0500

    - Al parecer una correccion definitiva para el tema de la codificación ᕕ( ᐛ )ᕗ

--- src/helpers/Row.php
+++ src/helpers/Row.php
@@ -9,13 +9,21 @@ class Row {
      */
     public function __construct(array $fields = null) {
         foreach ($fields as $field => $value) {
-            $this->$field = utf8_encode($value);
+            if(mb_check_encoding($value) == "UTF-8") {
+                $this->$field = $value;
+            } else {
+                $this->$field = utf8_encode($value);
+            }
         }
     }
 
     public function addField(array $field){
         foreach ($field as $_field => $value) {
+            if(mb_check_encoding($value) == "UTF-8") {
+                $this->$_field = $value;
+            } else {
                 $this->$_field = utf8_encode($value);
+            }
         }
     }
 }
