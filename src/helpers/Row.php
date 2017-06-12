16c76f02a4edcb2f0752fddbfdf1956f5ed6bb89
Ramiro Alvarez <liko28s@gmail.com>
2017-06-12 09:04:43 -0500

    - Otro intento con la codificacion - Personas, evitar redundancias ya que es el mismo model

--- src/helpers/Row.php
+++ src/helpers/Row.php
@@ -9,21 +9,13 @@ class Row {
      */
     public function __construct(array $fields = null) {
         foreach ($fields as $field => $value) {
-            if(mb_check_encoding($value) == "UTF-8") {
-                $this->$field = (string)$value;
-            } else {
-                $this->$field = utf8_encode($value);
-            }
+            $this->$field = (string) utf8_encode($value);
         }
     }
 
     public function addField(array $field){
         foreach ($field as $_field => $value) {
-            if(mb_check_encoding($value) == "UTF-8") {
-                $this->$_field = (string)$value;
-            } else {
-                $this->$_field = utf8_encode($value);
-            }
+            $this->$_field = (string) utf8_encode($value);
         }
     }
 }
