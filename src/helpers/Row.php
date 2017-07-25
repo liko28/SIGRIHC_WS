f023be616f8646ebf021f8f2ccc9a571c64322f2
Ramiro Alvarez <liko28s@gmail.com>
2017-07-25 08:55:25 -0500

    - Una mejor alternativa a la codificacion de caracteres t(-_-t)

--- src/helpers/Row.php
+++ src/helpers/Row.php
@@ -9,13 +9,13 @@ class Row {
      */
     public function __construct(array $fields = null) {
         foreach ($fields as $field => $value) {
-            $this->$field = mb_detect_encoding($value) == "UTF-8" ? $value : (string) utf8_encode($value);
+            $this->$field = mb_detect_encoding($value,'UTF-8',true) == true ? $value : (string) utf8_encode($value);
         }
     }
 
     public function addField(array $field){
         foreach ($field as $_field => $value) {
-            $this->$_field = mb_detect_encoding($value) == "UTF-8" ? $value : (string) utf8_encode($value);
+            $this->$_field = mb_detect_encoding($value,'UTF-8',true) == true ? $value : (string) utf8_encode($value);
         }
     }
 }
