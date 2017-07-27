5570a883527bb1d4b58bfbad609fc4589678963d
Ramiro Alvarez <liko28s@gmail.com>
2017-07-27 09:39:42 -0500

    - Nuevas columnas en SIGRI_VARIABLES, lo que evita un Join Inecesario.

--- src/models/VariableModel.php
+++ src/models/VariableModel.php
@@ -9,17 +9,6 @@ class VariableModel extends BaseModel {
         parent::__construct($connection);
         $this->setTableName('SIGRI_VARIABLES');
         $this->setPrimaryKey('ID_VARIABLE');
-        $this->addColumns('ID_VARIABLE', 'NOMBRE_VARIABLE', 'DESCRIPCION', 'ENTIDAD', 'ATRIBUTO', 'TIPOCAMPO', 'LONCAMPO', 'DEPENDE', 'OBLIGATORIO', 'ID_LISTA', 'NOMLISTA', 'VALORLISTA', 'ID_MODULO', 'TIPO', 'VALIDAR', 'EDADINI', 'EDADFIN', 'GENERO', 'ESTADO', 'VISIBILIDAD', 'NIVEL', 'CODIGO', 'ORDEN', 'FECCREA', 'FECMODI');
+        $this->addColumns('ID_VARIABLE', 'NOMBRE_VARIABLE', 'DESCRIPCION', 'ENTIDAD', 'ATRIBUTO', 'TIPOCAMPO', 'LONCAMPO', 'DEPENDE', 'OBLIGATORIO', 'ID_LISTA', 'NOMLISTA', 'VALORLISTA', 'ID_MODULO', 'TIPO', 'VALIDAR', 'EDADINI', 'EDADFIN', 'GENERO', 'ESTADO', 'VISIBILIDAD', 'NIVEL', 'CODIGO', 'ORDEN', 'FECCREA', 'FECMODI', 'INTERVALO', 'FRECUENCIA');
     }
-
-    /** @return CustomArray */
-    public function getAll(){
-        return $this->query("SELECT {$this->getColumns('ID_VARIABLE', 'NOMBRE_VARIABLE', 'DESCRIPCION', 'ENTIDAD', 'ATRIBUTO', 'TIPOCAMPO', 'LONCAMPO', 'DEPENDE', 'OBLIGATORIO', 'ID_LISTA', 'NOMLISTA', 'VALORLISTA', 'ID_MODULO', 'TIPO', 'VALIDAR', 'EDADINI', 'EDADFIN', 'GENERO', 'ESTADO', 'VISIBILIDAD', 'NIVEL', 'CODIGO', 'ORDEN', 'FECCREA', 'FECMODI')->commaSep()}, CONF.INTERVALO, CONF.FRECUENCIA FROM {$this->getFullTableName()} LEFT JOIN {$this->getSchema()}.SIGRI_CONFIGURACION CONF ON CONF.ID_VARIABLE = {$this->getFullTableName()}.ID_VARIABLE");
-    }
-
-    /** @return CustomArray */
-    public function getUpdates($lastSyncDate) {
-        return $this->query("SELECT {$this->getColumns('ID_VARIABLE', 'NOMBRE_VARIABLE', 'DESCRIPCION', 'ENTIDAD', 'ATRIBUTO', 'TIPOCAMPO', 'LONCAMPO', 'DEPENDE', 'OBLIGATORIO', 'ID_LISTA', 'NOMLISTA', 'VALORLISTA', 'ID_MODULO', 'TIPO', 'VALIDAR', 'EDADINI', 'EDADFIN', 'GENERO', 'ESTADO', 'VISIBILIDAD', 'NIVEL', 'CODIGO', 'ORDEN', 'FECCREA', 'FECMODI')->commaSep()}, CONF.INTERVALO, CONF.FRECUENCIA FROM {$this->getFullTableName()} LEFT JOIN {$this->getSchema()}.SIGRI_CONFIGURACION CONF ON CONF.ID_VARIABLE = {$this->getFullTableName()}.ID_VARIABLE WHERE {$this->getFullTableName()}.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP",$lastSyncDate);
-    }
-
 }
