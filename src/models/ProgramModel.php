23c5ab79ca55a798d599321983bffd5982c8c7ef
Ramiro Alvarez <liko28s@gmail.com>
2017-08-15 11:02:07 -0500

    - Nuevo Program Model

--- src/models/ProgramModel.php
+++ src/models/ProgramModel.php
@@ -13,18 +13,8 @@ class ProgramModel extends BaseModel {
     /** @param Connection $connection */
     public function __construct(Connection $connection) {
         parent::__construct($connection);
-        $this->setTableName('AUDI_PROGRAMAS');
+        $this->setTableName('AUDIT_PROGRAMAS');
         $this->setPrimaryKey('ID');
-        $this->addColumns('ID', 'CODIGO', 'NOMBRE', 'ACTIVO', 'EDAD_MIN', 'EDAD_MAX', 'HOMBRE', 'MUJER', 'FRECUENCIA', 'INTERVALO', 'CODSERV', 'DESCRIPCION');
-    }
-
-    /**
-     * @param $lastSyncDate
-     * @return \SIGRI_HC\Helpers\CustomArray
-     * @deprecated
-     */
-    public function getUpdates($lastSyncDate)
-    {
-        return parent::getUpdates($lastSyncDate);
+        $this->addColumns('ID', 'CODIGO', 'NOMBRE', 'ESTADO', 'EDAD_INICIAL', 'EDAD_FINAL', 'GENERO', 'FRECUENCIA', 'INTERVALO', 'CODSERV', 'DESCRIPCION');
     }
 }
