d8659bcdef31e4e03482db31acab286a7c46e3e1
Ramiro Alvarez <liko28s@gmail.com>
2017-08-24 16:39:56 -0500

    - Correccion con las Fechas de Option para Updates

--- src/models/OptionModel.php
+++ src/models/OptionModel.php
@@ -24,7 +24,7 @@ class OptionModel extends BaseModel {
     public function updatesForDemandas(\DateTime $lastSyncDate){
         $date = $lastSyncDate->format('Y-m-d-H.i.s');
         try {
-            $res = $this->query("SELECT DISTINCT {$this->getColumns()->commaSep()} FROM {$this->getFullTableName()} JOIN {$this->getSchema()}.DEMANDA_PREGUNTAS D ON D.ID_LISTA =  {$this->getFullTableName()}.ID_LISTA WHERE {$this->getFullTableName()}.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP OR D.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP;", $date,$date);
+            $res = $this->query("SELECT DISTINCT {$this->getColumns()->commaSep()} FROM {$this->getFullTableName()} JOIN {$this->getSchema()}.DEMANDA_PREGUNTAS D ON D.ID_LISTA =  {$this->getFullTableName()}.ID_LISTA WHERE {$this->getFullTableName()}.FECMODI BETWEEN '?' AND CURRENT_TIMESTAMP OR D.FECMODI BETWEEN '?' AND CURRENT_TIMESTAMP;", $date,$date);
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
@@ -43,7 +43,7 @@ class OptionModel extends BaseModel {
     public function updatesForAuditorias(\DateTime $lastSyncDate){
         $date = $lastSyncDate->format('Y-m-d-H.i.s');
         try {
-            $res = $this->query("SELECT DISTINCT {$this->getColumns()->commaSep()} FROM {$this->getSchema()}.SIGRI_OPCIONES JOIN {$this->getSchema()}.AUDIT_PREGUNTAS AP ON AP.ID_VARIABLE IS NOT NULL JOIN {$this->getSchema()}.SIGRI_VARIABLES SV ON SV.ID_LISTA = {$this->getFullTableName()}.ID_LISTA AND AP.ID_VARIABLE = SV.ID_VARIABLE WHERE {$this->getFullTableName()}.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP OR AP.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP OR SV.FECMODI BETWEEN ? AND CURRENT_TIMESTAMP;", $date, $date);
+            $res = $this->query("SELECT DISTINCT {$this->getColumns()->commaSep()} FROM {$this->getSchema()}.SIGRI_OPCIONES JOIN {$this->getSchema()}.AUDIT_PREGUNTAS AP ON AP.ID_VARIABLE IS NOT NULL JOIN {$this->getSchema()}.SIGRI_VARIABLES SV ON SV.ID_LISTA = {$this->getFullTableName()}.ID_LISTA AND AP.ID_VARIABLE = SV.ID_VARIABLE WHERE {$this->getFullTableName()}.FECMODI BETWEEN '?' AND CURRENT_TIMESTAMP OR AP.FECMODI BETWEEN '?' AND CURRENT_TIMESTAMP OR SV.FECMODI BETWEEN '?' AND CURRENT_TIMESTAMP;", $date, $date, $date);
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
